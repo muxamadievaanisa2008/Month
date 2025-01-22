574d2f0695b12ced8cbf258891a63b1108b6325f
Anisa <anisamuxamadieva@icloud.com>
2025-01-22 21:40:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let month = +prompt("Какое число?")
+
+switch (month) {
+    case 1:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 2:
+        console.log("В этом месяцу 28 день ");     
+        break;
+
+    case 3:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 4:
+        console.log("В этом месяцу 30 день");     
+        break;
+    case 5:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 6:
+        console.log("В этом месяцe 30 день");     
+        break;
+
+    case 7:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 8:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 9:
+        console.log("В этом месяцe 30 день");     
+        break;
+
+    case 10:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    case 11:
+        console.log("В этом месяцe 30 день");     
+        break;
+
+    case 12:
+        console.log("В этом месяцe 31 день");     
+        break;
+
+    default:
+        console.log("Такого числа нет!");       
+        break;
+}
